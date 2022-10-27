be6713b64127098b6d54819bd856e98e15ea561e
araikazuaki0504 <kaba_hipop@iCloud.com>
2022-10-27 15:03:56 +0900

    初級クラス課題

---
+++ 初級クラス課題/問題集/3-2/ConsoleApp1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("名前を入力し、Enterキーを押してください");
+            string inputDate = Console.ReadLine();
+
+            Console.WriteLine("年齢を入力し、Enterキーを押してください。");
+            string inputName = Console.ReadLine();
+
+            Console.WriteLine(inputDate);
+            Console.WriteLine(inputName);
+
+
+            Console.ReadLine();
+                
+
+        }
+    }
+}
