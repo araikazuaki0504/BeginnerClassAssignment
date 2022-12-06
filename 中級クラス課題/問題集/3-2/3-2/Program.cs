223eb1917137e55633d72e064042b577d4aad70b
araikazuaki0504 <kaba_hipop@iCloud>
2022-12-06 23:40:49 +0900

    12/6

---
+++ 中級クラス課題/問題集/3-2/3-2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _3_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("5の階乗 = " + Fact(5).ToString());
+            Console.WriteLine("10の階乗 = " + Fact(10).ToString());
+            Console.WriteLine("20の階乗 = " + Fact(20).ToString());
+        }
+
+        static long Fact(long PreviousNum)
+        {
+            if (PreviousNum == 0)
+            {
+                return 1;
+            }
+            else
+            {
+                return Fact(PreviousNum - 1) * PreviousNum;
+            }
+        }
+    }
+}
---
+++ 中級クラス課題/問題集/3-3/3-3/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace _3_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array1 = { 3, 5, 2, 7, 8, 3, 9, 6 };
+            int[] array2 = { 12, 24, 63, 11, 29 };
+            int[] array3 = { 1251, 3567, 9399, 6241 };
+
+            Console.WriteLine("array1の合計は" + Sum(array1).ToString() + "です。");
+            Console.WriteLine("array2の合計は" + Sum(array2).ToString() + "です。");
+            Console.WriteLine("array3の合計は" + Sum(array3).ToString() + "です。");
+        }
+
+        static int Sum(int[] Num)
+        {
+            int ans = 0;
+
+            foreach (int num in Num)
+            {
+                ans += num;
+            }
+
+            return ans;
+        }
+    }
+}
