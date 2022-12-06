df1d8664807c8cb5c2661c0915f116ed6154e684
araikazuaki0504 <kaba_hipop@iCloud>
2022-12-06 16:37:15 +0900

    12/6

---
+++ 中級クラス課題/問題集/3-1/3-1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _3_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] Person = { "A", "B" };
+            string[] Greeting = { "こんにちは", "こんばんは" };
+
+            for (int i = 0; i < 2; i++)
+            {
+                for (int j = 0; j < 2; j++)
+                {
+                    OutPutStrings(Person[j], Greeting[i]);
+                }
+            }
+
+        }
+
+        static void OutPutStrings(string person, string greeting)
+        {
+            Console.WriteLine(person + "さん");
+            Console.WriteLine(greeting);
+        }
+    }
+}
