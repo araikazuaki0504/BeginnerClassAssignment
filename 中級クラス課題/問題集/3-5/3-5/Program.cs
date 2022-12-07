193faf430e4cc5f12eb3bcc40c2fb99e9dd95364
araikazuaki0504 <kaba_hipop@iCloud>
2022-12-07 12:23:17 +0900

    12/7

---
+++ 中級クラス課題/問題集/3-5/3-5/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+
+namespace _3_5
+{
+    class Program
+    {
+        static private List<int> Data = new List<int>();
+        
+        static void Main(string[] args)
+        {
+            string FileName = @"C:\data\figureRow.txt";
+            ReadData(FileName);
+            int ans = SumList();
+
+            Console.WriteLine("合計は、" + ans.ToString() + "です"); 
+
+        }
+
+        static void ReadData(string FilePath)
+        {
+            StreamReader Reader = new StreamReader(FilePath);
+            
+            while (-1 < Reader.Peek())
+            {
+                string[] buff = Reader.ReadLine().Split(" ");
+                foreach (string data in buff)
+                {
+                    Data.Add(int.Parse(data));
+                }
+            }
+
+            Reader.Close();
+        }
+
+        static int SumList()
+        {
+            int ans = 0;
+            foreach (int num in Data)
+            {
+                ans += num;
+            }
+
+            return ans;
+        }
+    }
+}
