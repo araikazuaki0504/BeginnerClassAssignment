b413f59693b7495b16f00e93f2d62a005f275a74
araikazuaki0504 <kaba_hipop@iCloud.com>
2023-03-27 12:31:34 +0900

    3/27

--- 中級クラス課題/ドリル/問題29/問題29/Form_Main.Designer.cs
+++ 中級クラス課題/ドリル/問題29/問題29/Form_Main.Designer.cs
@@ -48,7 +48,6 @@
             ClientSize = new Size(242, 161);
             Controls.Add(button);
             Name = "Form_Main";
-            Text = "問題29";
             ResumeLayout(false);
         }
 
